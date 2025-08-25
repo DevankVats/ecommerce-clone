0b3d9ff7f901549e8c9aec3a905a96450d0e1406
DevankVats <vatsdevank2610@gmail .com>
2025-08-25 15:31:07 +0530

    use module and import cart from cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-const cart=[];
+export const cart=[];
+/*
+1.CREATE A MODULE 
+a.) create a file
+b.)don't load the filw with <script></script>
+NOTE: any varibale we create inside the file,will be contained inside the file,does'nt conflicts with another file name.
+
+2.GET A VARIABLE OUT OF A FILE
+a.)add type="module" attribute
+b.)Export
+c.)Import
+
+3. CONDITION FOR MODULES TO DO TASK
+a.) all items should be imported in begining.
+b.)modules only operates through live server
+  ADVANTAGES
+  a.) it helps us avoid naming conflicts.
+  b.)don't have to worry about orders of file.
+
+*/
